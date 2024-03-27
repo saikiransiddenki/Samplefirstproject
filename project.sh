1f6958d111f61ad3aa0f25fc4166a5641e550b14
saikiransiddenki <161606638+saikiransiddenki@users.noreply.github.com>
2024-03-27 10:38:09 +0530

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,3 +1,3 @@
 echo "This is my Jenkins fullcourse Tutorial. This is my first-1 Testing" 
-welcome to first commit
-welcome to second -version
+echo "welcome to first commit"
+echo "welcome to second -version"
